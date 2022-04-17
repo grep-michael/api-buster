d9189caaa27186e8a080f14d311895e61a6e7b5a
michaelknudsen <mknudsen762@gmail.com>
2022-04-17 01:47:56 -0400

    start

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/michaelknudsen/api-buster
+
+go 1.13
+
+require github.com/michaelknudsen/WordListReader v0.0.0-20220416194849-357b1adc0796
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+
+	WordListReader "github.com/michaelknudsen/WordListReader/wordlistreader"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wlr := WordListReader.MakeNewWordListReader("rockyou.txt")
+	defer wlr.Close()
+	wg.Add(1)
+	go sendReq(&wg, &wlr, 1)
+	wg.Add(1)
+	go sendReq(&wg, &wlr, 2)
+	wg.Add(1)
+	go sendReq(&wg, &wlr, 3)
+	wg.Wait()
+}
+
+func sendReq(wg *sync.WaitGroup, wlr *WordListReader.WordListReader, id int) {
+	defer wg.Done()
+	methods := [...]string{"GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "CONNECT", "OPTIONS", "TRACE"}
+	for word := range wlr.Iter() {
+		fmt.Println(id, word)
+		for _, v := range methods {
+			reader := strings.NewReader("Test Body")
+			r, _ := http.NewRequest(v, "http://localhost:8080/"+word, reader)
+			client := http.Client{}
+			client.Do(r)
+		}
+
+	}
+}
